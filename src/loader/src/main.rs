3ee77d615be8f9802abeacdb4378e77bd69efaa5
evpost <kripsy93@yandex.ru>
2021-04-20 00:30:38 +0300

    add register

--- src/loader/src/main.rs
+++ src/loader/src/main.rs
@@ -1,38 +1,103 @@
 use sha2::{Sha256, Sha512, Digest};
+use std::str::FromStr;
+use serde::{Serialize, Deserialize};
+use std::mem::swap;
+use reqwest::StatusCode;
 
-fn main() {
-    let find = b"\xAB\xAB\xAB\xAB";
-    let s64 = b"sha256 ends with AA:AA:AA:AA if ";
 
-    println!("{}", u32::from_be_bytes(find.to_owned()));
+#[derive(Serialize, Deserialize)]
+struct Challenge {
+    pub bytes: String,
+    pub nonce: i32
+}
 
-    return;
+impl Challenge {
+    pub fn check(&self, solution: i32) -> bool {
 
-    let mut i = 0;
-    loop {
-
-        // create a Sha256 object
         let mut hasher = Sha256::new();
-
-        // write input message
-        hasher.update(s64);
-
-        // ...
-        hasher.update(i32::to_be_bytes(i));
-
-        // ...
+        hasher.update(&self.bytes);
+        hasher.update(i32::to_be_bytes(solution));
         let result = hasher.finalize();
+        result.ends_with(&i8::to_be_bytes(self.nonce as i8))
+        //result.ends_with(&i8::to_be_bytes(self.nonce))
 
-        // ...
-        if result.ends_with(find) {
-            println!("found for {} retries! sha256({:?} + {:?}) endswith BB:BB:BB:BB", i, s64, i32::to_be_bytes(i));
-            break;
+    }
+
+    pub fn find_solution(&self) -> i32 {
+
+        let mut i = 0;
+        loop {
+            if self.check(i) {
+                return i;
+            }
+            i += 1;
         }
 
-        if i % 10_000_000 == 0 {
-            println!("{}", i);
-        }
-
-        i += 1;
     }
 }
+
+#[derive(Serialize, Deserialize)]
+struct ChallengeResponse {
+    status: String,
+    challenge: Challenge
+}
+
+#[derive(Serialize, Deserialize)]
+struct RegisterRequest {
+    challenge: Challenge,
+    solution: i32
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct RegisterResponse {
+    pub status: String,
+    pub token: String
+}
+
+fn main() {
+
+    let ip_address = "http://localhost:8000";
+
+    let challenge_response = get_challenge(ip_address).unwrap();
+    let find_solution = challenge_response.challenge.find_solution();
+
+    let register_result = register(ip_address, challenge_response, find_solution).unwrap();
+
+    let a = 3;
+
+    return;
+}
+
+fn get_challenge(ip_address: &str) -> Result<ChallengeResponse, reqwest::Error> {
+
+    let client = reqwest::blocking::Client::new();
+    let resp = client.get(format!("{}/challenge", ip_address))
+        .send();
+
+    resp?.json()
+}
+
+fn register(ip_address: &str, challenge_response: ChallengeResponse, solution: i32) -> Result<RegisterResponse, reqwest::Error> {
+
+    let client = reqwest::blocking::Client::new();
+
+    let register_request = RegisterRequest {
+        challenge: challenge_response.challenge,
+        solution
+    };
+
+    let resp = client.post( format!("{}/register", ip_address))
+        .json(&register_request)
+        .send();
+
+    let a = resp.unwrap().text().unwrap();
+
+    let t = 0;
+
+    panic!("aaa");
+    //resp?.json();
+}
+
+
+
