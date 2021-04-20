30676ceca017ac482849488dd42fe16579a8c14f
evpost <kripsy93@yandex.ru>
2021-04-20 23:16:36 +0300

    create Api_Server struct

--- src/loader/src/main.rs
+++ src/loader/src/main.rs
@@ -3,101 +3,61 @@ use std::str::FromStr;
 use serde::{Serialize, Deserialize};
 use std::mem::swap;
 use reqwest::StatusCode;
+use shared::{challenge::Challenge, api::*};
 
+struct ApiServer {
 
-#[derive(Serialize, Deserialize)]
-struct Challenge {
-    pub bytes: String,
-    pub nonce: i32
-}
-
-impl Challenge {
-    pub fn check(&self, solution: i32) -> bool {
-
-        let mut hasher = Sha256::new();
-        hasher.update(&self.bytes);
-        hasher.update(i32::to_be_bytes(solution));
-        let result = hasher.finalize();
-        result.ends_with(&i8::to_be_bytes(self.nonce as i8))
-        //result.ends_with(&i8::to_be_bytes(self.nonce))
-
-    }
-
-    pub fn find_solution(&self) -> i32 {
-
-        let mut i = 0;
-        loop {
-            if self.check(i) {
-                return i;
-            }
-            i += 1;
-        }
-
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct ChallengeResponse {
-    status: String,
-    challenge: Challenge
-}
-
-#[derive(Serialize, Deserialize)]
-struct RegisterRequest {
-    challenge: Challenge,
-    solution: i32
-}
-
-
-#[derive(Serialize, Deserialize)]
-struct RegisterResponse {
-    pub status: String,
+    pub ip_address: String,
     pub token: String
+}
+
+impl ApiServer {
+
+    fn new(ip_address: &str) -> ApiServer {
+        ApiServer { ip_address: ip_address.to_string(), token: "".to_string() }
+    }
+
+    pub fn get_challenge(&self) -> Result<ChallengeResponse, reqwest::Error> {
+
+        let client = reqwest::blocking::Client::new();
+        let resp = client.get(format!("{}/challenge", &self.ip_address))
+            .send();
+
+        resp?.json()
+    }
+
+    pub fn register(&self, challenge_response: ChallengeResponse, solution: i32) -> Result<RegisterResponse, reqwest::Error> {
+
+        let client = reqwest::blocking::Client::new();
+
+        let register_request = RegisterParameters {
+            token: challenge_response.token,
+            solution
+        };
+
+        let resp = client.post( format!("{}/register", self.ip_address))
+            .json(&register_request)
+            .send();
+
+
+        resp?.json()
+    }
+
+
 }
 
 fn main() {
 
     let ip_address = "http://localhost:8000";
 
-    let challenge_response = get_challenge(ip_address).unwrap();
-    let find_solution = challenge_response.challenge.find_solution();
+    let mut api_server = ApiServer::new(ip_address);
+    let challenge_response = api_server.get_challenge().unwrap(); //get_challenge(ip_address).unwrap();
+    let solution = challenge_response.challenge.solve();
+    api_server.token = api_server.register(challenge_response, solution).unwrap().token;
 
-    let register_result = register(ip_address, challenge_response, find_solution).unwrap();
 
     let a = 3;
 
     return;
 }
-
-fn get_challenge(ip_address: &str) -> Result<ChallengeResponse, reqwest::Error> {
-
-    let client = reqwest::blocking::Client::new();
-    let resp = client.get(format!("{}/challenge", ip_address))
-        .send();
-
-    resp?.json()
-}
-
-fn register(ip_address: &str, challenge_response: ChallengeResponse, solution: i32) -> Result<RegisterResponse, reqwest::Error> {
-
-    let client = reqwest::blocking::Client::new();
-
-    let register_request = RegisterRequest {
-        challenge: challenge_response.challenge,
-        solution
-    };
-
-    let resp = client.post( format!("{}/register", ip_address))
-        .json(&register_request)
-        .send();
-
-    let a = resp.unwrap().text().unwrap();
-
-    let t = 0;
-
-    panic!("aaa");
-    //resp?.json();
-}
-
-
 
